348f370e57f2d8e95c5de8d224b49556e0a2c752
JNettli <jonas.nettli@gmail.com>
2025-03-06 14:13:14 +0100

    Fixed spacing issue on main page

--- src/App.tsx
+++ src/App.tsx
@@ -40,23 +40,28 @@ function App() {
             <h1 className="text-4xl font-semibold flex justify-center pb-4">
                 Browse All Products
             </h1>
-            <div className="bg-slate-200 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {data.map((product) => {
-                    return (
-                        <div className="flex justify-center" key={product.id}>
-                            <Card
-                                id={product.id}
-                                title={product.title}
-                                image={product.image}
-                                price={product.price}
-                                discountedPrice={product.discountedPrice}
-                                description={product.description}
-                                rating={product.rating}
-                                reviews={product.reviews}
-                            />
-                        </div>
-                    );
-                })}
+            <div className="max-w-max mx-auto">
+                <div className="bg-slate-200 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                    {data.map((product) => {
+                        return (
+                            <div
+                                className="flex justify-center"
+                                key={product.id}
+                            >
+                                <Card
+                                    id={product.id}
+                                    title={product.title}
+                                    image={product.image}
+                                    price={product.price}
+                                    discountedPrice={product.discountedPrice}
+                                    description={product.description}
+                                    rating={product.rating}
+                                    reviews={product.reviews}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </>
     );
